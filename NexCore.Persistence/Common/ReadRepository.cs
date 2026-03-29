19fcde7f4d784a8d76a75152d94b9f251808a893
Adrian <cristofer.calcinas@uab.edu.bo>
2026-03-29 12:08:12 -0400

    feat(persistence): add ReadRepository and Repository classes for data access

---
+++ NexCore.Persistence/Common/ReadRepository.cs
@@ -0,0 +1,8 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using NexCore.Application.Common.Interfaces;
+
+namespace NexCore.Persistence.Common;
+
+public class ReadRepository<T>(AppReadDbContext dbContext) :
+    RepositoryBase<T>(dbContext), IReadRepository<T>
+    where T : class;
---
+++ NexCore.Persistence/Common/Repository.cs
@@ -0,0 +1,8 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using NexCore.Application.Common.Interfaces;
+
+namespace NexCore.Persistence.Common;
+
+public class Repository<T>(AppDbContext dbContext)
+    : RepositoryBase<T>(dbContext), IRepository<T>
+    where T : class;
